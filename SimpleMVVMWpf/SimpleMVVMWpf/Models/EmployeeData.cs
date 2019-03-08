471a584dfe93f4c8692849953564456b22ca0a59
Mohanvel V <mohannvell@gmail.com>
2019-03-08 15:25:19 +0530

    Add files via upload

---
+++ SimpleMVVMWpf/SimpleMVVMWpf/Models/EmployeeData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleMVVMWpf.ViewModels;
+
+namespace SimpleMVVMWpf.Models {
+    public class EmployeeData : BaseViewModel {
+        private string _Name;
+        public string Name {
+            get {
+                return _Name;
+            }
+            set {
+                _Name = value;
+
+            }
+        }
+
+        private string _Email;
+        public string Email {
+            get {
+                return _Email;
+            }
+            set {
+                _Email = value;
+            }
+        }
+
+        private string _Department;
+        public string Department {
+            get {
+                return _Department;
+            }
+            set {
+                _Department = value;
+            }
+        }
+
+    }
+}
---
+++ SimpleMVVMWpf/SimpleMVVMWpf/ViewModels/EmployeeDataViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using SimpleMVVMWpf.Models;
+
+namespace SimpleMVVMWpf.ViewModels {
+    public class EmployeeDataViewModel : BaseViewModel {
+        private EmployeeData _newInput = new EmployeeData();
+        public EmployeeData NewInput {
+            get {
+                return _newInput;
+            }
+            set {
+                _newInput = value;
+                NotifyPropertyChanged("NewInput");
+            }
+        }
+
+        //List to display the added items
+        private ObservableCollection<EmployeeData> _EmployeeList;
+        public ObservableCollection<EmployeeData> EmployeeList {
+            get {
+                return _EmployeeList;
+            }
+            set {
+                _EmployeeList = value;
+            }
+        }
+
+        public EmployeeDataViewModel() {
+            _EmployeeList = new ObservableCollection<EmployeeData>();
+            _EmployeeList.Add(new EmployeeData() { Name = "Mohan", Department = "IT", Email = "mohan@gmail.com" });
+            _EmployeeList.Add(new EmployeeData() { Name = "Vel", Department = "Testing", Email = "vel@gmail.com" });
+
+            //Adds a new item to the collection
+            AddToMaster = new RelayCommand(param => {
+                _EmployeeList.Add(NewInput);
+                NewInput = new EmployeeData();
+            });
+
+            //Removes selected item from collection
+            DeleteFromMaster = new RelayCommand(param => {
+                _EmployeeList.Remove(ItemToDelete);
+            });
+        }
+
+        public EmployeeData ItemToDelete { get; set; } //property to hold selected item
+
+        public ICommand AddToMaster { get; set; } //Command to add a new item
+        public ICommand DeleteFromMaster { get; set; } //Command to delete an item
+    }
+}
